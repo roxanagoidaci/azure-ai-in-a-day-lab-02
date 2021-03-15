2aae5bb234fcc7c56ff2879540a1d0cc15735d7e
Roxana Goidaci <roxana.goidaci@genisoft.eu>
2021-03-15 16:10:56 +0200

    Update load_sample_data.py

--- ml_service/pipelines/load_sample_data.py
+++ ml_service/pipelines/load_sample_data.py
@@ -1,19 +1,13 @@
 
-import pandas as pd
-from azureml.core import Workspace, Experiment, Dataset, VERSION
+from azureml.core import Workspace, Dataset
 
 # Loads the COVID articles sample data from dataset COVID19Articles_Test.
 def create_sample_data_csv(file_name: str = "metadata_clusters.csv",
                            for_scoring: bool = False):
+  
     ws = Workspace.from_config()
     sample_data = Dataset.get_by_name(ws, 'COVID19Articles_Test')
-    columns_to_ignore = ['sha', 'source_x', 'title', 'doi', 'pmcid', 'pubmed_id', 'license', 'abstract', 'publish_time', 'authors', 'journal', 'mag_id',
-                     'who_covidence_id', 'arxiv_id', 'pdf_json_files', 'pmc_json_files', 'url', 's2_id' ]
-    sample_data = sample_data.drop_columns(columns_to_ignore) 
-    
     df = sample_data.to_pandas_dataframe()
-    
     if for_scoring:
         df = df.drop(columns=['cluster'])
-
     df.to_csv(file_name, index=False)
