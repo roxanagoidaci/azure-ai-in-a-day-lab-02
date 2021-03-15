2df58e7e4ac8082308edb99fde09032e7f9d7034
Roxana Goidaci <roxana.goidaci@genisoft.eu>
2021-03-15 14:47:25 +0200

    Update load_sample_data.py

--- ml_service/pipelines/load_sample_data.py
+++ ml_service/pipelines/load_sample_data.py
@@ -3,16 +3,16 @@ import pandas as pd
 from sklearn.datasets import load_diabetes
 
 
-# Loads the diabetes sample data from sklearn and produces a csv file that can
+# Loads the COVID articles sample data from sklearn and produces a csv file that can
 # be used by the build/train pipeline script.
-def create_sample_data_csv(file_name: str = "diabetes.csv",
+def create_sample_data_csv(file_name: str = "metadata_clusters.csv",
                            for_scoring: bool = False):
-    sample_data = load_diabetes()
+    sample_data = load_COVID19articles()
     df = pd.DataFrame(
         data=sample_data.data,
         columns=sample_data.feature_names)
     if not for_scoring:
-        df['Y'] = sample_data.target
+        df['cluster'] = sample_data.target
     # Hard code to diabetes so we fail fast if the project has been
     # bootstrapped.
     df.to_csv(file_name, index=False)
