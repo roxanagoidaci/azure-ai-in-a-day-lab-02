a776c6236b255a12e62528002cd68c8d1e01c4e0
Roxana Goidaci <roxana.goidaci@genisoft.eu>
2021-03-16 16:33:50 +0200

    line too long fix

--- ml_service/pipelines/load_sample_data.py
+++ ml_service/pipelines/load_sample_data.py
@@ -1,12 +1,14 @@
-from azureml.core import Workspace, Dataset
+import pandas as pd
 
 
 # Loads the COVID articles sample data from dataset COVID19Articles_Test.
 def create_sample_data_csv(file_name: str = "COVID19Articles.csv",
                            for_scoring: bool = False):
 
-    sample_data = Dataset.File.from_files('https://solliancepublicdata.blob.core.windows.net/ai-in-a-day/lab-02/COVID19Articles.csv')
-    df = sample_data.to_pandas_dataframe()
+    url = \
+        "https://solliancepublicdata.blob.core.windows.net" + \
+        "/ai-in-a-day/lab-02/"
+    df = pd.read_csv(url + file_name)
     if for_scoring:
         df = df.drop(columns=['cluster'])
     df.to_csv(file_name, index=False)
